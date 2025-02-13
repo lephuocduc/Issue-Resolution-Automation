cff86fc4847fd3953f9bb8c87c1a79ac6f6e41a0
GitHub Action <action@github.com>
2025-02-13 05:17:18 +0000

    Add ScriptManager.exe

--- ScriptManager.ps1
+++ ScriptManager.ps1
@@ -48,7 +48,7 @@ $main_form.Controls.Add($label)
 $comboBox = New-Object System.Windows.Forms.ComboBox
 #$comboBox.Location = New-Object System.Drawing.Point(110, 50)  # Centered horizontally - REMOVE THIS LINE
 $comboBox.Size = New-Object System.Drawing.Size (200, 25) # set the size of combobox
-$comboBox.Items.AddRange(@('LowFreeSpace','Script1','Script2','Script3','Script4','Script5','Script6'))  # Add items to the dropdown
+$comboBox.Items.AddRange(@('LowFreeSpace','Script1','Script2','Script3'))  # Add items to the dropdown
 $comboBox.DropDownStyle = 'DropDown' # Allow text editing in the ComboBox
 # Calculate the horizontal center for the ComboBox
 $combobox_width = $comboBox.Size.Width
@@ -116,15 +116,6 @@ $okButton.Add_Click({
         "Script3" {
             . (Join-Path $PSScriptRoot 'Scripts\Script3.ps1')
         }
-        "Script4" {
-            . (Join-Path $PSScriptRoot 'Scripts\Script4.ps1')
-        }
-        "Script5" {
-            . (Join-Path $PSScriptRoot 'Scripts\Script5.ps1')
-        }
-        "Script6" {
-            . (Join-Path $PSScriptRoot 'Scripts\Script6.ps1')
-        }
         default {
             [System.Windows.Forms.MessageBox]::Show(
                 "No script is associated with the selection '$selectedValue'.",
@@ -161,5 +152,6 @@ $main_form.Controls.Add($cancelButton)
 
 # Show the form as a dialog
 $main_form.ShowDialog()
+
 
 

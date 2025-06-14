0b1b1cba32b806011dd585d6225ab14e66be4e95
Duc Le <leducst1@gmail.com>
2025-06-14 10:27:12 +0700

    performance issue notes, ideas, workflow, functions

---
+++ Scripts/Performance Issue/Performance Issue.ps1
@@ -0,0 +1,6 @@
+[System.Windows.Forms.MessageBox]::Show(
+    "The script hasn't been done yet. Welcome you to the project! ", 
+    "Information", 
+    [System.Windows.Forms.MessageBoxButtons]::OK, 
+    [System.Windows.Forms.MessageBoxIcon]::Information
+)

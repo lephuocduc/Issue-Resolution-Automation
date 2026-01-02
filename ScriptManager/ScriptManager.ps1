a6fe82d174da439b4ec7b119344d22284a42f9ba
Duc Le <leducst1@gmail.com>
2026-01-02 08:58:27 +0700

    commit

--- ScriptManager/ScriptManager.ps1
+++ ScriptManager/ScriptManager.ps1
@@ -3,22 +3,35 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 # Import all the modules !@#$%^
-. (Join-Path $PSScriptRoot "..\Modules")
-<#. (Join-Path $PSScriptRoot "..\Modules\Compress-IISLogs.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Export-DiskReport.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Get-DiskSpaceDetails.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Get-PerformanceMetrics.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Get-Session.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Get-SystemUptime.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Get-TopCPUProcesses.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Get-TopItems.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Get-TopMemoryProcesses.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Show-PerformanceDashboard.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Test-DiskAvailability.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Test-ReportFileCreation.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Test-ServerAvailability.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Write-Log.psm1")
-. (Join-Path $PSScriptRoot "..\Modules\Write-WindowsEventLog.psm1")#>
+$ModuleList = @(
+    "Clear-SystemCache.psm1",
+    "Compress-IISLogs.psm1",
+    "Export-DiskReport.psm1",
+    "Get-DiskSpaceDetails.psm1",
+    "Get-PerformanceMetrics.psm1",
+    "Get-Session.psm1",
+    "Get-SystemUptime.psm1",
+    "Get-TopCPUProcesses.psm1",
+    "Get-TopItems.psm1",
+    "Get-TopMemoryProcesses.psm1",
+    "Show-PerformanceDashboard.psm1",
+    "Test-DiskAvailability.psm1",
+    "Test-ReportFileCreation.psm1",
+    "Test-ServerAvailability.psm1",
+    "Write-Log.psm1",
+    "Write-WindowsEventLog.psm1"
+)
+
+# Loop through the list and dot-source each one using your path logic
+foreach ($ModuleFile in $ModuleList) {
+    $ModulePath = Join-Path $PSScriptRoot "..\Modules\$ModuleFile"
+    if (Test-Path $ModulePath) {
+        . $ModulePath
+    }
+    else {
+        Write-Warning "Required module not found: $ModulePath"
+    }
+}
 
 
 # Import the Get-BitwardenAuthentication module
@@ -342,7 +355,6 @@ if ($script:ADM_Credential -and $script:JumpHost) {
     # Show the main form after Bitwarden authentication
     $main_form.ShowDialog()
 }
-
 
 
 

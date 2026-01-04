17cadd62b47e3ad45d13ffb69a7e9ef046973538
GitHub Action <action@github.com>
2026-01-04 12:44:12 +0000

    Add ScriptManager.exe

--- ScriptManager/ScriptManager.ps1
+++ ScriptManager/ScriptManager.ps1
@@ -1,4 +1,4 @@
-﻿# Load the necessary assembly for Windows Forms
+# Load the necessary assembly for Windows Forms
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
@@ -440,6 +440,8 @@ if ($script:ADM_Credential -and $script:JumpHost) {
     # Show the main form after Bitwarden authentication
     $main_form.ShowDialog()
 }
+
+
 
 
 
